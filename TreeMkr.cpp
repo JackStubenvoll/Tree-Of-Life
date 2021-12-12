e525d93e0d9a322b098a9e8939905a2e967a5d2c
Jack Stubenvoll <jackts2@linux-37.ews.illinois.edu>
2021-12-12 00:15:40 -0600

    testing

---
+++ TreeMkr.cpp
@@ -0,0 +1,119 @@
+#include "TreeMkr.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <queue>
+using namespace std;
+Tree::Tree(){
+    ifstream inputfile;
+    string textfile = "AnimalKingdom.txt";
+    inputfile.open(textfile);
+    while(getline(inputfile, temp)){
+        for(int i = 0; i < 1593758; i++){
+            storage[i] = temp;
+        }
+        int tmp = 0;
+        cout<< "got line " << temp << " " << endl;
+        tmp++;
+    }
+    //read array into tree structure
+    root = new Node(0);
+    Node* currKingdom;
+    Node* currPhylum;
+    Node* currClass;
+    Node* currOrder;
+    Node* currFamily;
+    Node* currGenus;
+    for (unsigned i = 0; i < 1593758; i++) {
+        std::string line = storage[i];
+        size_t startInd = line.find("[");
+        size_t endInd = line.find("]");
+        std::string level = line.substr(startInd, endInd - startInd);
+        switch (level):
+            case "kingdom":
+                //only kingdom is the first line
+                currKingdom = root;
+                break;
+            case "phylum":
+                //Create node representing phylum
+                //add phylum to list of children of most recent kingdom
+                //set this phylum as most recent
+                Node* newPhylum(i);
+                currKingdom->child.push_back(newPhylum);
+                currPhylum = newPhylum;
+                break;
+            case "class":
+                //create node representing class
+                //add class to lst of children of most recent phylum
+                //set this class as most recent
+                Node* newClass(i);
+                currPhylum->child.push_back(newClass);
+                currClass = newClass;
+                break;
+            case "order":
+                //create node representing order
+                //add order to lst of children of most recent class
+                //set this order as most recent
+                Node* newOrder(i);
+                currClass->child.push_back(newOrder);
+                currOrder = newOrder;
+                break;
+            case "family":
+                //create node representing family
+                //add family to lst of children of most recent order
+                //set this family as most recent
+                Node* newFamily(i);
+                currOrder->child.push_back(newFamily);
+                currFamily = newFamily;
+                break;
+            case "genus":
+                //create node representing genus
+                //add genus to lst of children of most recent family
+                //set this genus as most recent
+                Node* newGenus(i);
+                currFamily->child.push_back(newGenus);
+                currGenus = newGenus;
+                break;
+            case "species":
+                //create node representing species
+                //add species to lst of children of most recent genus
+                //nothing below species
+                Node* newSpecies(i);
+                currGenus->child.push_back(newSpecies);
+                break;
+        
+    }
+}
+
+string Tree::mostConnected(Node* root) {
+    int largest = root->child.size();
+    Node* mostConnectedNode = root;
+    std::queue<Node*> q;
+    q.push(root);
+    while (!q.empty()) {
+        Node* temp = q.front();
+        if (temp->child.size() > largest) {
+            largest = temp->child.size();
+            mostConnectedNode = temp;
+        }
+        for (unsigned i = 0; i < temp->child.size(); i++) {
+            q.push(temp->child.at(i));
+        }
+
+    }
+    return storage[mostConnectedNode->elements];
+} 
+
+Node* Tree::findNode(string name) {
+    std::queue<Node*> q;
+    q.push(root);
+    while (!q.empty()) {
+        Node* temp = q.front();
+        if (storage[temp->element].find(name) != string::npos)
+        for (unsigned i = 0; i < temp->child.size(); i++) {
+            q.push(temp->child.at(i));
+        }
+
+    }
+}
---
+++ TreeMkr.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+class Tree{
+    struct Node{
+        int elements;
+        vector<Node*> child;
+        //default constructor
+        Node();
+        Node(int elem){
+          elements = elem;
+        }
+        Node(const Node& other):elements(other.elements){}
+        Node* parent;
+    };
+  public:
+    Tree();
+    Tree(const Tree& other);
+    ~Tree();
+    const Tree& operator=(const Tree& rhs);
+    string mostConnected(Node* root);
+    Node* findNode(string Name);
+  private:
+    Node* root;
+    void clear();
+    Node* copy(const Node* subroot);
+    string temp;
+    string storage[1593758];
+};
